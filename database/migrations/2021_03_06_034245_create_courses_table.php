f128e3d246dbdc79787a777e5285a0503da4749e
Axel Sarceño <asarceno@mintrabajo.gob.gt>
2021-03-05 21:48:37 -0600

    inicio con migraciones

---
+++ database/migrations/2021_03_06_034245_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->text('description');
+            $table->enum('status', [1,2,3])->default(1)->comment('1 = borrador, 2 = revision');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
